a7147051d1c48dc12799dc55c2839f07b14569f8
Elias <eliasvillamayorprofesional@gmail.com>
2024-07-22 16:08:21 -0300

    restauracion

---
+++ GraphicMarket/src/test/java/com/graphicmarket/GraphicMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphicmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
